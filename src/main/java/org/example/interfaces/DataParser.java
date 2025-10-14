5dcf238b60d69bdb7fec6e335c43cae43c603b77
TwillightMoon <47419205+IPaleLuna@users.noreply.github.com>
2025-10-14 19:36:03 +0300

    Удаление лишних классов

--- src/main/java/org/example/interfaces/DataParser.java
+++
@@ -1,5 +0,0 @@
-package org.example.interfaces;
-
-public interface DataParser<T> {
-    T parse(String data);
-}
--- src/main/java/org/example/interfaces/DataValidator.java
+++
@@ -1,5 +0,0 @@
-package org.example.interfaces;
-
-public interface DataValidator<T> {
-    boolean isValid(T data);
-}
